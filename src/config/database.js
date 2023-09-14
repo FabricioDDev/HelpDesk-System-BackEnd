03978366d0e0850bdc56fb5cd8752524d5d9f642
Ferwin Arias <ferwin.arias@gmail.com>
2023-09-14 12:18:34 -0500

    Depuring Consoles

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,6 @@
 const { Sequelize } = require('sequelize');
 const { dbName, dbPass, dbUser, dbHost, dbDialect } = require('./index');
 
-console.log(dbName, dbPass, dbUser, dbHost, dbDialect)
-
 const db = new Sequelize(dbName, dbUser, dbPass, {
     host: dbHost,
     dialect: dbDialect, 
